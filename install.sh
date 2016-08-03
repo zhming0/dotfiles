a2b1c1a384b4c1e13aa99aa54226f6914047c7fc
Zhiming Guo <mjzshd@gmail.com>
2016-08-03 23:13:45 +1000

    Added dir variable

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+BASEDIR="$(pwd)"
+
 # Vim
 mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-ln -s .vimrc ~
+ln -s {BASEDIR}/.vimrc ~
 
 # Fish
 mkdir -p ~/.config/fish/functions/
-ln -s fish_prompt.fish ~/.config/fish/functions
+ln -s {BASEDIR}/fish_prompt.fish ~/.config/fish/functions
