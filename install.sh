f8c6a30d5848f7283b2906c0a8511870915f8294
Zhiming Guo <mjzshd@gmail.com>
2016-08-03 22:56:52 +1000

    Added fish_prompt and other things

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Vim
+mkdir -p ~/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s .vimrc ~
+
+# Fish
+mkdir -p ~/.config/fish/functions/
+ln -n fish_prompt.fish ~/.config/fish/functions
