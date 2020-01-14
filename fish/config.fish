3406f36ab504e64c576a93290a84c141a9137c7b
Zhiming Guo <guo.zhming@gmail.com>
2020-01-15 10:42:08 +1100

    tweak to fzf default command

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-# PG's CLI tools 
+# PG's CLI tools
 # https://stackoverflow.com/a/49689589
 set -xg PATH /usr/local/Cellar/libpq/11.5/bin $PATH
 
@@ -17,5 +17,5 @@ set -xg EDITOR nvim
 
 # Let FZF to use FD instaed of FIND by default
 # so gitignore is respected
-set -xg FZF_DEFAULT_COMMAND 'fd --type f'
+set -xg FZF_DEFAULT_COMMAND 'fd --hidden --type f'
 set -xg FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
