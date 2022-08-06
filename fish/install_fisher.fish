bf80c46545e795958d611c19d1c517fb6a626a07
zhming0 <guo.zhming@gmail.com>
2022-08-06 20:05:50 +1000

    codify tide configuration

--- fish/install_fisher.fish
+++ fish/install_fisher.fish
@@ -1,4 +1,8 @@
 #!/usr/bin/env fish
 
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-fisher install ilanCosman/tide@v5
+
+fisher update
+
+# Check https://github.com/IlanCosman/tide/issues/304 to see if there is a better way.
+echo 2 1 2 2 1 1 1 2 1 3 3 2 2 y | tide configure >/dev/null
