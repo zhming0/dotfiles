c4952998c84f74cac77a117112b0ee442949d579
zhming0 <guo.zhming@gmail.com>
2023-10-02 09:48:16 +1100

    use telescope to view lsp references + minor cleanup

--- nvim/lua/lsp_setup.lua
+++ nvim/lua/lsp_setup.lua
@@ -119,19 +119,23 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local opts = { buffer = ev.buf }
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    --  I could use the original quickfix based solution but telescope looks better :)
+    -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', 'gr', "<cmd>Telescope lsp_references<cr>", opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', '<leader>k', vim.lsp.buf.signature_help, opts)
+
     -- Not sure what these are
     ---vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
     ---vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
     ---vim.keymap.set('n', '<space>wl', function()
     ---  print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     ---end, opts)
-    vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
+
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
     vim.keymap.set('n', '<leader>=', function()
       vim.lsp.buf.format { async = true }
     end, opts)
