375cd289b6c90f71517cbb2ec9df407fd2c787cd
zhming0 <guo.zhming@gmail.com>
2024-02-20 22:14:06 +1100

    minor clean up

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ if [[ $(brew --version) ]] ; then
 else
     echo "Attempting to install Homebrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 brew install \
@@ -70,15 +71,12 @@ brew install \
   podman \
   difftastic
 
-# brew install --cask qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv qlimagesize webpquicklook quicklookase qlvideo
-
 asdf plugin add nodejs || true
 asdf plugin add java || true
 asdf plugin add terraform || true
 
-# 16.0.0 support both intel and arm Mac
-asdf install nodejs 16.10.0
-asdf global nodejs 16.10.0
+asdf install nodejs 20.11.1
+asdf global nodejs 20.11.1
 
 asdf install java openjdk-21
 asdf global java openjdk-21
@@ -86,8 +84,8 @@ asdf global java openjdk-21
 asdf install terraform latest
 asdf global terraform latest
 
-bash ./fish/setup.sh
-bash ./nvim/setup.sh
+./fish/setup.sh
+./nvim/setup.sh
 ./rust/setup.sh
 ./tmux/setup.sh
 ./go/setup.sh
@@ -98,7 +96,5 @@ bash ./nvim/setup.sh
 helm plugin install https://github.com/databus23/helm-diff
 helm plugin install https://github.com/jkroepke/helm-secrets --version v4.1.1
 helm plugin install https://github.com/helm/helm-mapkubeapis
-
-./bin/gh-cargo-install.sh --git crate-ci/typos --target x86_64-apple-darwin --tag v1.12.12
 
 exec -l $SHELL
