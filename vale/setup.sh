4a3b19e1ef732aa3772158dd222c2e4bf07b62f5
Zhiming Guo <guo.zhming@gmail.com>
2024-04-10 23:55:52 +1000

    fix vale problem and add a simple tech vocab file

--- vale/setup.sh
+++ vale/setup.sh
@@ -6,9 +6,13 @@ set -o nounset
 
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __vale_config_path="$__dir/vale.ini"
+__vale_style_path="$HOME/.local/share/vale/styles"
+__vale_tech_voca_dir="$__vale_style_path/config/vocabularies/Tech"
 
-mkdir -p ~/.local/share/vale/
+mkdir -p "$__vale_style_path"
+mkdir -p "$__vale_tech_voca_dir"
 
 ln -sfv "$__vale_config_path" ~/.vale.ini
+ln -sfv "$__dir/accept.txt" "$__vale_tech_voca_dir/accept.txt"
 
 vale sync --config="$__vale_config_path"
