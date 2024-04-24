77da68d02483989d6e521f00caf2edfd68e0e926
Zhiming Guo <guo.zhming@gmail.com>
2024-04-24 11:15:37 +1000

    upgrade neovim plugins

--- nvim/lua/lsp_setup.lua
+++ nvim/lua/lsp_setup.lua
@@ -12,7 +12,6 @@ require("mason-lspconfig").setup{
     "yamlls", "bashls",
     "lua_ls", -- Only need this for init.lua for now..
     "jdtls",
-    "ruby_ls",
     "vale_ls",
     "gopls",
     "golangci_lint_ls",
