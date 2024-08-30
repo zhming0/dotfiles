4bf5db28b730589bc00453c2f6b521ed807eaba5
Zhiming Guo <guo.zhming@gmail.com>
2024-08-30 22:46:49 +1000

    upgrade codecompanion and barbar

--- nvim/lua/codecompanion_setup.lua
+++ nvim/lua/codecompanion_setup.lua
@@ -1,39 +1,5 @@
 local vim = vim
 
-local send_txt = function(context)
-  local text = require("codecompanion.helpers.actions").get_code(context.start_line, context.end_line)
-
-  return "I have the following text/code:\n\n```" .. context.filetype .. "\n" .. text .. "\n```\n\n"
-end
-
-local generate_action_item = function(name, prompt)
-  return {
-    name = name,
-    strategy = "inline",
-    description = name,
-    opts = {
-      modes = { "v" },
-    },
-    prompts = {
-      {
-        role = "system",
-        content = "You are an expert writer/coder and helpful assistant who can help writing and coding. Your writing style is concise and impactful, without any unnecessary fluff.",
-      },
-      {
-        role = "user",
-        contains_code = true,
-        content = function(context)
-          return send_txt(context)
-        end,
-      },
-      {
-        role = "user",
-        content = prompt .. " Only return the improved writing or code without any wraps.",
-      },
-    },
-  }
-end
-
 return {
   "olimorris/codecompanion.nvim",
   dependencies = {
@@ -49,11 +15,12 @@ return {
       },
     },
   },
+  version = "*",
   event = "VeryLazy",
   config = function ()
     require("codecompanion").setup({
       adapters = {
-        openai = require("codecompanion.adapters").use("openai", {
+        openai = require("codecompanion.adapters").extend("openai", {
           env = {
             api_key = "OPENAI_API_KEY",
           },
@@ -68,26 +35,6 @@ return {
           adapter = "openai",
         },
       },
-
-      -- This doesn't work yet.
-      prompts = {
-        ["Whaaaat"] = {
-        }
-      },
-
-      actions = {
-        {
-          name = "Ming Writing",
-          strategy = "inline",
-          description = "Help writing and coding in general, Ming's custom actions.",
-          picker = {
-            items = {
-              generate_action_item("Improve writing", "Please improve writing for me while respecting the existing format and meaning."),
-              generate_action_item("Grammar", "Please fix grammar and spelling for me."),
-            },
-          },
-        },
-      }
     })
 
     vim.api.nvim_set_keymap("n", "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
