1b56849ccdbaddfa2419078274da8728ba964630
Ming Guo <guo.zhming@gmail.com>
2024-09-04 14:50:47 +1000

    nvim: tweak pesisted setup

--- nvim/lua/persisted_setup.lua
+++ nvim/lua/persisted_setup.lua
@@ -18,13 +18,23 @@ return {
         local uv = vim.loop
         local cwd = uv.cwd()
         local git_dir = uv.fs_stat(cwd .. "/.git")
+        if git_dir == nil then
+          return false
+        end
 
         -- Check if the current buffer is a GIT COMMIT message buffer
         local current_buf = vim.api.nvim_get_current_buf()
         local buf_name = vim.api.nvim_buf_get_name(current_buf)
         local is_git_commit = buf_name:match("COMMIT_EDITMSG$") ~= nil
+        if is_git_commit then
+          return false
+        end
 
-        return git_dir ~= nil and not is_git_commit
+        if vim.fn.argc() > 0 then
+          return false
+        end
+
+        return true
       end
     })
 
