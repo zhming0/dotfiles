ac2db589acf9e84c7e0cbb6584c15c2b447a0579
Zhiming Guo <guo.zhming@gmail.com>
2025-12-26 20:48:52 +1100

    disable auto repl from conjure

--- nvim/lua/clojure_related_setup.lua
+++ nvim/lua/clojure_related_setup.lua
@@ -4,16 +4,16 @@ vim.g["conjure#log#hud#height"] = 1
 vim.g["conjure#log#hud#width"] = 1
 vim.g["conjure#log#log#wrap"] = "true"
 -- Disable Conjure's K shortcut in favor of LSP's support
--- TODO need test
 vim.g["conjure#mapping#doc_word"] = false
+vim.g["conjure#client#clojure#nrepl#connection#auto_repl#enabled"] = false
 
 -- We have baleia to render color!
 vim.g["conjure#log#strip_ansi_escape_sequences_line_limit"] = 0
 
 -- disable auto-pair for clojure in favor of parinfer
-vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
-  pattern = {"*.clj", "*.cljs"},
-  callback = function ()
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = { "*.clj", "*.cljs" },
+  callback = function()
     vim.g.AutoPairsShortcutToggle = false
   end
 })
